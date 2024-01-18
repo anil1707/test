53695ea97e243ea23e0d61c833e0d8ee0d4285ec
Anil Kumar Yadav <yadavanil1707ax@gmail.com>
2024-01-19 03:20:51 +0530

    clear client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect, useState } from "react";
+
+import {  useState } from "react";
 
 function App() {
   const [email,setEmail] = useState("")
